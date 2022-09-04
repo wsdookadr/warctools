576b31d3909078e8e80c9dd575636ab4ae492f2e
Stefan Petrea <stefan.petrea@gmail.com>
2022-09-04 09:00:25 +0300

    update

--- bin/warc_resources.py
+++ bin/warc_resources.py
@@ -36,7 +36,7 @@ if __name__ == '__main__':
     arg_parser.add_argument('--infile', dest='infile', action='store', required=True, help='input WARC')
     arg_parser.add_argument('--get-latex-imgs', dest='get_latex_imgs', action='store_true', required=False, help='switch to extract latex image urls')
     arg_parser.add_argument('--imgs', dest='imgs', action='store_true', required=False, help='get images matching substring')
-    arg_parser.add_argument('--url-filter', dest='url_filter', action='store', required=False, help='regex filter for web pages')
+    arg_parser.add_argument('--url-filter', dest='url_filter', action='store', required=False, help='filter for warc web pages to extract links from')
     arg_parser.add_argument('--pdfs', dest='pdfs', action='store_true', required=False, help='grab pdf urls')
     arg_parser.add_argument('--js', dest='js', action='store_true', required=False, help='grab script urls')
     arg_parser.add_argument('--css', dest='css', action='store_true', required=False, help='grab css style urls')
@@ -100,7 +100,7 @@ if __name__ == '__main__':
 
                 if args.links:
                     for e in r.xpath('//a'):
-                        if 'href' in e.attrib:
+                        if 'href' in e.attrib and re.match(r'^http',e.attrib['href']):
                             print(e.attrib['href'])
 
 
