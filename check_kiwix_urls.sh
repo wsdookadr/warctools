309501ce527f52ad46c2f03ffe4bb6d3ab2c1a5e
Stefan Petrea <stefan.petrea@gmail.com>
2024-08-03 09:05:35 +0300

    update

--- check_kiwix_urls.sh
+++ check_kiwix_urls.sh
@@ -3,12 +3,13 @@
 # post-processing to assess which pages did not make it into the warc
 # 
 URL_FILE="$1"
-IP=$(ip route get 1 | perl -ne 'm{src (\d+\.\d+\.\d+\.\d+)} && print "$1\n";')
+#IP=$(ip route get 1 | perl -ne 'm{src (\d+\.\d+\.\d+\.\d+)} && print "$1\n";')
 http_status() {
     RAW_URL="$1"
     URL=$(echo "$1" | perl -pne 's{^https?://}{}g;')
-    STATUS=$(curl -I -s -o /dev/null -w "%{http_code}"  "http://$IP:8083/content/big/$URL")
+    STATUS=$(curl -I -s -o /dev/null -w "%{http_code}"  "http://192.168.1.150:8083/content/big/$URL")
     echo -e "$STATUS\t$RAW_URL"
 }
 export -f http_status
-cat "$URL_FILE" | parallel --no-notice --progress -j6 -k http_status "{1}" ::: | awk '$1 != 200 { print $2; }' > not_found_in_kiwix.txt
+#cat "$URL_FILE" | parallel --no-notice --progress -j6 -k http_status "{1}" ::: # | awk '$1 != 200 { print $2; }' > not_found_in_kiwix.txt
+cat "$URL_FILE" | parallel --progress --no-notice -j6 -k http_status "{1}" ::: | awk '$1 != 200 { print $2; }' | tee not_found_in_kiwix.txt
--- fetch_missing.sh
+++ fetch_missing.sh
@@ -3,9 +3,6 @@ mkdir missing_warc/
 rm -f missing_warc/*
 cp missing.txt missing_warc/list.txt
 cd missing_warc/
-i=0
-while read URL; do
-    i=$((i+1))
-    URL=$(echo "$URL" | perl -pne 's{\r\n}{}g;s{\n}{}g;')
-    wget --no-warc-compression --warc-file $i -O/dev/null "$URL"
-done < list.txt
+
+cat list.txt | perl -pne 's{\r\n}{\n}g;' | \
+shuf | parallel --progress --no-notice -j4 'echo "{1}"; wget -q --no-warc-compression --warc-file {#} -O/dev/null "{=uq=}"' :::
